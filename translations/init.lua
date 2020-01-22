1db438f7747f684a13a3c581fd1b8072366f11fe
Iván Gabriel <tocutoeltuco@gmail.com>
2020-01-22 15:18:55 -0300

    Remove global translations

--- translations/init.lua
+++ translations/init.lua
@@ -1,11 +1,6 @@
-local translations, global_translations
+local translations
 translations = setmetatable({}, {
 	__index = function()
 		return translations.en
-	end
-})
-global_translations = setmetatable({}, {
-	__index = function()
-		return global_translations.en
 	end
 })
