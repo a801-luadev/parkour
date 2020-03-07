81042479f61a4b051557c90a21e5662f9a12cba3
Iván Gabriel <tocutoeltuco@gmail.com>
2020-03-07 15:50:53 -0300

    [build] Make the events faster. Closes #6

--- global/event-handler.lua
+++ global/event-handler.lua
@@ -5,7 +5,6 @@ local onEvent
 do
 	local os_time = os.time
 	local math_floor = math.floor
-	local table_unpack = table.unpack
 	local runtime_check = 0
 	local events = {}
 	local scheduled = {_count = 0, _pointer = 1}
@@ -14,23 +13,23 @@ do
 	local _paused = false
 
 	local function runScheduledEvents()
-		local ret_val = true
 		local count, pointer = scheduled._count, scheduled._pointer
 
 		local data
 		while pointer <= count do
 			data = scheduled[pointer]
-			data[1](table_unpack(data, 2))
+			-- An event can have up to 5 arguments. In this case, this is faster than table.unpack.
+			data[1](data[2], data[3], data[4], data[5], data[6])
 			pointer = pointer + 1
 
-			if runtime > runtime_threshold then
+			if runtime >= runtime_threshold then
 				scheduled._count = count
-				ret_val = false
-				break
+				scheduled._pointer = pointer
+				return false
 			end
 		end
 		scheduled._pointer = pointer
-		return ret_val
+		return true
 	end
 
 	local function emergencyShutdown(limit_players)
@@ -58,19 +57,32 @@ do
 			evt = {_count = 0}
 			events[name] = evt
 
+			-- An event can have up to 5 arguments. In this case, this is faster than `...`
+			local function caller(when, a, b, c, d, e)
+				for index = 1, evt._count do
+					evt[index](a, b, c, d, e)
+
+					if os_time() >= when then
+						break
+					end
+				end
+			end
+
 			local schedule = name ~= "Loop" -- schedule everything but eventLoop
 			local done, result
 			local event_fnc
-			event_fnc = function(...)
+			event_fnc = function(a, b, c, d, e)
 				local start = os_time()
-				local max_runtime = runtime_threshold - runtime
+				local max_runtime = start + runtime_threshold - runtime
 				local this_check = math_floor(start / 4000)
-				if runtime_check ~= this_check then
+				if runtime_check < this_check then
 					runtime_check = this_check
 					runtime = 0
 					paused = false
 
 					if not runScheduledEvents() then
+						runtime_check = this_check + 1
+						paused = true
 						return
 					end
 
@@ -78,24 +90,18 @@ do
 						translatedChatMessage("resumed_events")
 						_paused = false
 					end
-				elseif paused then
+				elseif paused and schedule then
 					scheduled._count = scheduled._count + 1
-					scheduled[scheduled._count] = {event_fnc, ...}
+					scheduled[scheduled._count] = {event_fnc, a, b, c, d, e}
 					return
 				end
 
-				for index = 1, evt._count do
-					done, result = pcall(evt[index], ...)
-					if not done then
-						local args = json.encode({...})
-						translatedChatMessage("code_error", nil, name, index, args, result)
+				done, result = pcall(caller, max_runtime, a, b, c, d, e)
+				if not done then
+					local args = json.encode({a, b, c, d, e})
+					translatedChatMessage("code_error", nil, name, index, args, result)
 
-						return emergencyShutdown(true)
-					end
-
-					if (os_time() - start) > max_runtime then
-						break
-					end
+					return emergencyShutdown(true)
 				end
 
 				runtime = runtime + (os_time() - start)
@@ -105,6 +111,7 @@ do
 						translatedChatMessage("paused_events")
 					end
 
+					runtime_check = this_check + 1
 					paused = true
 					_paused = true
 					scheduled._count = 0
