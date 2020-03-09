9005128cf08802c45053121a6e23264b15ac36f3
Iván Gabriel <tocutoeltuco@gmail.com>
2020-03-09 12:20:03 -0300

    [build] Set maxplayers to 50 in the maps room.

--- modes/maps/init.lua
+++ modes/maps/init.lua
@@ -2,6 +2,6 @@ if tfm.get.room.name ~= "*#parkour0maps" then
 	translatedChatMessage("maps_not_available")
 	emergencyShutdown(true)
 else
-	tfm.exec.setRoomMaxPlayers(50)
 	eventGameStart()
+	tfm.exec.setRoomMaxPlayers(50)
 end
