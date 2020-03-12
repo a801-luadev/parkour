2a5d60b35c974777fa95b4a36a133f37931d929c
Iván Gabriel <tocutoeltuco@gmail.com>
2020-03-12 19:36:44 -0300

    Add a launcher for the bots

---
+++ bots/launcher.py
@@ -0,0 +1,24 @@
+import subprocess
+import signal
+
+while True:
+	print("Launching bot...")
+	process = subprocess.Popen(["python3.7", "run.py"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	try:
+		while True:
+			line = process.stdout.readline()
+			if not line:
+				break
+
+			if "Restarting transformice bot" == line:
+				break
+	except KeyboardInterrupt:
+		break
+	finally:
+		print("Stopping with CTRL+C...")
+		process.send_signal(signal.SIGINT)
+		try:
+			process.wait(timeout=10.0)
+		except subprocess.TimeoutExpired:
+			print("Can not stop the process with CTRL+C. Killing it.")
+			process.kill()
