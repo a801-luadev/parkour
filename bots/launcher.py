0a9d0651186fc418ac74c4c13ee19c5a8f8c4dd5
Iván Gabriel <tocutoeltuco@gmail.com>
2020-03-14 16:09:44 -0300

    Add an error handler if something fails.

--- bots/launcher.py
+++ bots/launcher.py
@@ -3,26 +3,30 @@ import signal
 import time
 
 while True:
-	print("Launching bot...")
-	process = subprocess.Popen(["python3.7", "run.py"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	try:
-		while True:
-			line = process.stdout.readline()
-			if not line:
-				break
-
-			print(line, end="")
-			if "Restarting transformice bot" == line.rstrip():
-				break
-	except KeyboardInterrupt:
-		break
-	finally:
-		print("Stopping with CTRL+C...")
-		process.send_signal(signal.SIGINT)
+		print("Launching bot...")
+		process = subprocess.Popen(["python3.7", "run.py"], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		try:
-			process.wait(timeout=10.0)
-		except subprocess.TimeoutExpired:
-			print("Can not stop the process with CTRL+C. Killing it.")
-			process.kill()
+			while True:
+				line = process.stdout.readline()
+				if not line:
+					break
 
-		time.sleep(3.0)
+				print(line, end="")
+				if "Restarting transformice bot" == line.rstrip():
+					break
+		except KeyboardInterrupt:
+			break
+		finally:
+			print("Stopping with CTRL+C...")
+			process.send_signal(signal.SIGINT)
+			try:
+				process.wait(timeout=10.0)
+			except subprocess.TimeoutExpired:
+				print("Can not stop the process with CTRL+C. Killing it.")
+				process.kill()
+
+			time.sleep(3.0)
+	except:
+		print("Could not launch bot.")
+		time.sleep(15.0)
