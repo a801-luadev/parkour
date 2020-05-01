5acc5b9041ef9f20221554820ac8d4b38e00aa21
Iván Gabriel <tocutoeltuco@gmail.com>
2020-05-01 13:50:43 -0300

    Update aiotfmpatch

--- bots/transformice/aiotfmpatch.py
+++ bots/transformice/aiotfmpatch.py
@@ -3,46 +3,7 @@ import asyncio
 import random
 
 class Client(aiotfm.Client):
-	async def connect(self):
-		"""|coro|
-		Creates a connection with the main server.
-		"""
-
-		for port in random.sample([13801, 11801, 12801, 14801], 4):
-			try:
-				await self.main.connect('51.75.130.180', port)
-			except Exception:
-				pass
-			else:
-				break
-		else:
-			raise aiotfm.errors.ServerUnreachable('Unable to connect to the server.')
-
-		while not self.main.open:
-			await asyncio.sleep(.1)
-	
-	async def restart(self, keys=None):
-		"""Restarts the client.
-		:param keys:"""
+	async def restart(self, *args, **kwargs):
 		print("Restarting transformice bot", flush=True)
 
-		self.dispatch("restart")
-
-		try:
-			self.close()
-		except:
-			pass
-
-		try:
-			if keys is not None:
-				self.keys = keys
-			else:
-				self.keys = keys = await get_keys(self.api_tfmid, self.api_token)
-
-			self.main = aiotfm.Connection("main", self, self.loop)
-			self.bulle = None
-			await self.connect()
-			await self.sendHandshake()
-			await self.locale.load()
-		except:
-			asyncio.ensure_future(self.restart_soon(), loop=self.loop)
+		return await super().restart(*args, **kwargs)
