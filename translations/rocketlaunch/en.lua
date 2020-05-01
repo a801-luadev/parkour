c275353cc31c0950aa9f420e2b0c3b978f1d43c2
Iván Gabriel <tocutoeltuco@gmail.com>
2020-05-01 15:39:26 -0300

    Move freezertag and rocketlaunch

--- translations/rocketlaunch/en.lua
+++
@@ -1,6 +0,0 @@
-translations.en = {
-	you_are_shaman = "<VP>You are the shaman! Build a rocket to reach the space and save the crew members!</VP>",
-	you_are_crew_member = "<VP>You are a crewman! Wait for the Shaman to build his rocket to reach the space!</VP>",
-	welcome = "<D>Welcome to #rocketlaunch. The goal is to reach the space, so the shaman must build a ROCKET!</D>\n<CE>Credits for the original idea: Kralizmox#0000.</CE>",
-	remvove_floor = "<p align='center'><font size='20'><R>Removing the floor in %d seconds</R></font></p>"
-}
