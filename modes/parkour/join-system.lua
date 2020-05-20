fd65ce0285809c20b9abcfc85b825b6c632b4362
Iván Gabriel <tocutoeltuco@gmail.com>
2020-05-20 13:48:09 -0300

    Fix glitch of max players and (probably) consumables

--- modes/parkour/join-system.lua
+++ modes/parkour/join-system.lua
@@ -1,8 +1,10 @@
+local room_max_players = 12
+
 onEvent("PacketReceived", function(packet_id, packet)
 	if packet_id == 0 then
 		if packet == room.name then
-			tfm.exec.setRoomMaxPlayers(20)
-			addNewTimer(15000, tfm.exec.setRoomMaxPlayers, 12)
+			tfm.exec.setRoomMaxPlayers(room_max_players + 10)
+			addNewTimer(15000, tfm.exec.setRoomMaxPlayers, room_max_players)
 		end
 	end
 end)
