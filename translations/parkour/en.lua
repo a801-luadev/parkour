2a0ac944d5ae56618222c18e6cdd7ea82836957c
Iván Gabriel <tocutoeltuco@gmail.com>
2020-05-24 16:53:40 -0300

    typo

--- translations/parkour/en.lua
+++ translations/parkour/en.lua
@@ -82,7 +82,7 @@ translations.en = {
 	kill_map = "<R>Your powers have been disabled until next map.",
 
 	-- Miscellaneous
-	options = "<p align='center'><font size='20'>Parkour Options</font></p>\n\nUse particles for checkpoints\n\nUse <b>QWERTY</b> keyboard (disable if <b>AZERTY</b>)\n\nUse <b>M</b> hotkey for <b>/mort</b> (disable for <b>DEL</b>)\n\nShow your power cooldowns\n\nShow powers button\n\nShow help button\n\nShow map completion announcements\n\nShow no help indicator",
+	options = "<p align='center'><font size='20'>Parkour Options</font></p>\n\nUse particles for checkpoints\n\nUse <b>QWERTY</b> keyboard (disable if <b>AZERTY</b>)\n\nUse <b>M</b> hotkey for <b>/mort</b> (disable for <b>DEL</b>)\n\nShow your power cooldowns\n\nShow powers button\n\nShow help button\n\nShow map completion announcements\n\nShow no help symbol",
 	unknown = "Unknown",
 	powers = "Powers",
 	press = "<vp>Press %s",
