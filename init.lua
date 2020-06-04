693824455c3b186da4a816f0bac309e93c0a4e77
Iván Gabriel <tocutoeltuco@gmail.com>
2020-06-04 18:37:52 -0300

    [build] Finish refactoring

--- init.lua
+++ init.lua
@@ -32,7 +32,7 @@ local links = {
 	modapps = "https://bit.ly/parkourmod"
 }
 
-local starting = string.sub(tfm.get.room.name, 1, 2)
+local starting = string.sub(room.name, 1, 2)
 
 local tribe, module_name, submode
 local flags = ""
@@ -40,24 +40,28 @@ local flags = ""
 {% require-package "translations" %}
 {% require-package "global" %}
 
-if starting == "*\003" then
-	tribe = string.sub(tfm.get.room.name, 3)
-
+local function initialide_parkour() -- so it uses less space after building
 	{% require-package "modes/parkour" %}
+end
+
+if starting == "*\003" then
+	tribe = string.sub(room.name, 3)
+
+	initialide_parkour()
 else
 	local pos
 	if starting == "*#" then
-		module_name = string.match(tfm.get.room.name, "^%*#([a-z]+)")
+		module_name = string.match(room.name, "^%*#([a-z]+)")
 		pos = #module_name + 3
 	else
-		module_name = string.match(tfm.get.room.name, "^[a-z][a-z2]%-#([a-z]+)")
+		module_name = string.match(room.name, "^[a-z][a-z2]%-#([a-z]+)")
 		pos = #module_name + 5
 	end
 
 	local numbers
-	numbers, submode = string.match(tfm.get.room.name, "^(%d+)([a-z_]+)", pos)
+	numbers, submode = string.match(room.name, "^(%d+)([a-z_]+)", pos)
 	if numbers then
-		flags = string.sub(tfm.get.room.name, pos + #numbers + #submode + 1)
+		flags = string.sub(room.name, pos + #numbers + #submode + 1)
 	end
 
 	if room.name == "*#parkour4bots" then
@@ -67,10 +71,10 @@ else
 	elseif submode == "rocketlaunch" then
 		{% require-package "modes/rocketlaunch" %}
 	else
-		{% require-package "modes/parkour" %}
+		initialide_parkour()
 	end
 end
 
-for player in next, tfm.get.room.playerList do
+for player in next, room.playerList do
 	eventNewPlayer(player)
 end
