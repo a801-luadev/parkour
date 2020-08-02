3a1c3b941889fa0fe214ce3450fb19013b522dc7
Iván Gabriel <tocutoeltuco@gmail.com>
2020-08-02 13:25:09 -0300

    [build] modify texts

--- modes/parkour/interface.lua
+++ modes/parkour/interface.lua
@@ -761,8 +761,7 @@ onEvent("NewPlayer", function(player)
 	tfm.exec.lowerSyncDelay(player)
 
 	translatedChatMessage("welcome", player)
-	translatedChatMessage("staff_power", player)
-	translatedChatMessage("no_help", player)
+	translatedChatMessage("donate", player)
 
 	system.bindKeyboard(player, 38, true, true)
 	system.bindKeyboard(player, 40, true, true)
