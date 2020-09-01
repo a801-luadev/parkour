abaa70e8205757072b41238f06c626e5943188a6
Reza Juliandri <rezajuliandri20@gmail.com>
2020-09-01 12:43:30 +0700

    Add Bahasa Indonesia

---
+++ translations/parkour/id.lua
@@ -0,0 +1,81 @@
+translations.id = {
+    name = "id",
+    fullname = "Bahasa Indonesia",
+
+    -- Error messages
+    corrupt_map = "<r>Peta rusak. Memuat lainnya.",
+    corrupt_map_vanilla = "<r>[KESALAHAN] <n>Tidak bisa mendapatkan informasi dari peta ini.",
+    corrupt_map_mouse_start = "<r>[KESALAHAN] <n>Peta ini harus memiliki posisi awal (titik spawn tikus).",
+    corrupt_map_needing_chair = "<r>[KESALAHAN] <n>Peta harus memiliki kursi di akhir.",
+    corrupt_map_missing_checkpoints = "<r>[KESALAHAN] <n>Peta harus memiliki setidaknya satu checkpoin (paku kuning).",
+    corrupt_data = "<r>Sayangnya, data kamu rusak dan telah disetel ulang.",
+    min_players = "<r>Untuk menyimpan data kamu, setidaknya ada 4 pemain unik di ruangan ini. <bl>[%s/%s]",
+    tribe_house = "<r>Data tidak akan disimpan di rumah suku.",
+    invalid_syntax = "<r>Sintaks tidak valid.",
+    code_error = "<r>Terjadi kesalahan: <bl>%s-%s-%s %s",
+    emergency_mode = "<r>Memulai penghentian darurat, tidak ada pemain baru diizinkan. Mohon pergi ke ruangan #parkour lain.",
+    leaderboard_not_loaded = "<r>Papan peringkat belum dimuat. Tunggu sebentar.",
+
+    -- Help window
+    help = "Bantuan",
+    staff = "Staff",
+    rules = "Peraturan",
+    contribute = "Kontribusi",
+    changelog = "Berita",
+    help_help = "<p align = 'center'><font size = '14'>Selamat datang di ruangan <T>#parkour!</T></font></p>\n<font size = '12'><p align='center'><J>Tujuan kamu adalah meraih semua checkpoin sebelum menyelesaikan peta</J></p>\n\n<N>• Tekan <O>O</O>, tulis <O>!op</O> atau klik pada <O>tombol konfigurasi</O> untuk membuka <T>menu opsi</T>.\n• Tekan <O>P</O> atau klik <O>ikon tangan</O> pada kanan atas untuk membuka <T>menu kemampuan</T>.\n• Tekan <O>L</O> atau tulis <O>!lb</O> untuk membuka <T>papan peringkat</T>.\n• Tekan <O>M</O> atau tombol <O>Delete</O> untuk <T>/mort</T>, kamu bisa mengaktifkan tombol di menu <J>Opsi</J>.\n• Untuk mengetahui <O>staf</O> kami dan <O>aturan parkour</O>, klik pada tab <T>Staf</T> dan <T>Aturan</T>.\n• Klik <a href='event:discord'><o>disini</o></a> untuk mendapatkan tautan discord dan <a href='event:map_submission'><o>disini</o></a> untuk mendapatkan tautan topik mengenai pengajuan peta.\n• Gunakan <o>atas</o> dan <o>bawah</o> tombol panah ketika kamu ingin melakukan scroll.\n\n<p align = 'center'><font size = '13'><T>Kontribusi sekarang dibuka! Untuk detail lebih lanjut, klik pada tab<O>Kontribusi</O>!</T></font></p>",
+    help_staff = "<p align = 'center'><font size = '13'><r>DISCLAIMER: Staf parkour BUKAN staff Transformice dan TIDAK memiliki kemampuan apapun didalam game itu sendiri, hanya di dalam modul.</r>\nStaff parkur memastikan modul berjalan sempurna dengan masalah yang minim, dan akan selalu tersedia untuk membantu pemain kapan pun dibutuhkan.</font></p>\nKamu bisa menulis <D>!staff</D> di chat untuk melihat list staf.\n\n<font color = '#E7342A'>Admin:</font> Mereka bertanggung jawab dalam mengembangkan modul dengan menambahkan update baru ataupun memperbaiki bug.\n\n<font color = '#D0A9F0'>Manager Tim:</font> Mereka mengawasi tim dari moderator dan mappers untuk memastikan mereka mengerjakan perkerjaan mereka dengan baik. Mereka juga bertanggung jawab untuk merekrut anggota baru ke tim staf.\n\n<font color = '#FFAAAA'>Moderator:</font> Mereka bertanggung jawab untuk menegakkan ak=turan dari modul dan memberikan sanksi untuk individu yang tidak mengikutinya.\n\n<font color = '#25C059'>Mappers:</font> Mereka bertanggung jawab dalam meninjau, menambahkan, menghapus peta yang ada di modul untuk memastikan permainan yang menyenangkan.",
+    help_rules = "<font size='13'><B><J>Semua aturan dalam Syarat dan Ketentuan Transformice berlaku juga di #parkour</J></B></font>\n\nJika kamu menemukan pemain yang melanggar aturan tersebut, bisik moderator di game. Jika tidak ada moderator yang online, direkomendasikan untuk melaporkan melalui server discord. \nKetika melaporkan, mohon untuk melampirkan server, nama ruangan, dan nama pemain. \n• Contoh: en-#parkour10 Blank#3495 trolling\nBukti seperti tangkapan layar, video, dan gif sangat membantu dan dihargai, tetapi tidak perlu.\n\n<font size='11'>• Tidak ada <font color='#ef1111'>hacks, glitches or bugs</font> yang digunakan di ruangan #parkour\n•<font color='#ef1111'>VPN farming</font> akan dianggap sebagai <B>exploit</B> dan tidak diizinkan.<p align='center'><font color='#cc2222' size='12'><B>\nSiapapun yang ketahuan melanggar aturan ini akan segera diblokir.</B></font></p>\n\n<font size='12'>Transformice mengizinkan konsep trolling. Namun,<font color='#cc2222'><B>kami tidak mengizinkannya di parkour.</B></font></font>\n\n<p align='center'><J>Trolling adalah ketika seorang pemain dengan sengaja menggunakan kemampuan atau consumables untuk mencegah pemain lain dalam menyelesaikan peta.</j></p>\n• Trolling balas dendam<B> bukan sebuah alasan yang valid</B> untuk melakukan troll kepada seseorang dan kamu akan tetap diberi hukuman.\n• Memaksa membantu pemain yang mencoba untuk menyelesaikan peta sendirian dan menolak untuk berhenti melakukannya jika diminta juga termasuk sebagai trolling\n• <J>Jika seorang pemain tidak ingin bantuan dan lebih memilih solo, lebih baik membantu pemain yang lain</J>. Namun jika ada pemain lain yang meminta bantuan di checkpoin yang sama dengan pemain solo, kamu bisa membantu mereka [Keduanya].\n\nJika pemain tertangkap melakukan trolling, mereka akan mendapatkan hukuman berbasis waktu. Perlu diperhatikan bahwa trolling yang berulang akan mengakibatkan hukuman yang lebih lama dan lebih berat.",
+    help_contribute = "<font size='14'>\n<p align='center'>Tim amangemen parkur menyukai kode sumber terbuka karena itu  <t>membantu komunitas</t>. Kamu bisa <o>melihat</o> dan <o>memodifikasi</o> kode sumber dari <o><u><a href='event:github'>GitHub</a></u></o>.\n\nMemelihara modul <t>sepenuhnya bersifat sukarela</t>, sehingga bantuan mengenai <t>kode</t>, <t>laporan bug</t>, <t>saran</t> dan <t>pembuatan peta</t> selalu <u>diterima dan diharga</u>.\nKamu bisa <vp>melaporkan bug</vp> dan <vp>memberikan saran</vp> pada <o><u><a href='event:discord'>Discord</a></u></o> dan/atau <o><u><a href='event:github'>GitHub</a></u></o>.\nKamu bisa <vp>mengirimkan peta kamu</vp> di<o><u><a href='event:map_submission'>Forum Thread</a></u></o> kami.\n\nMemelihara parkur memang tidak mahal, tapi juga tidak gratis. Kami akan senang jika kamu bisa membantu kami dengan <t>berdonasi berapapun jumlahnya</t><o><u><a href='event:donate'>disini</a></u></o>.\n<u>Semua donasi akan digunakna untuk meningkatkan modul.</u></p>",
+    help_changelog = "<font size='13'><p align='center'><o>Versi 2.4.2 - 02/08/2020</o></p>\n\n• Memperbaiki beberapa bug yang merusak modul.\n• Memperbaiki bug dimana tidak mengizinkan seseorang melihat jumlah yang peta yang dibutuhkan untuk beberapa kemampuan.\n• Perintah <cep>!langue</cep>sekarang permanen.",
+
+    -- Congratulation messages
+    reached_level = "<d>Selamat! Kamu meraih level <vp>%s</vp>.",
+    finished = "<d><o>%s</o> menyelesaikan parkour dalam <vp>%s</vp> detik, <fc>selamat!",
+    unlocked_power = "<ce><d>%s</d> membuka kemampuan<vp>%s</vp>.",
+
+    -- Information messages
+    donate = "<vp>Tulis <b>!donate</b> jika kamu ingin berdonasi untuk modul ini!",
+    paused_events = "<cep><b>[Perhatian!]</b> <n>The module has reached it's critical limit and is being paused.",
+    paused_events = "<cep><b>[Perhatian!]</b> <n>Modul mencapai batas kritis dan akan dihentikan.",
+    resumed_events = "<n2>Modul terlah dilanjutkan.",
+    welcome = "<n>Selamat datang di <t>#parkour</t>!",
+    module_update = "<r><b>[Perhatian!]</b> <n>Modul akan diupdate dalam <d>%02d:%02d</d>.",
+    leaderboard_loaded = "<j>Papan peringkat sudah dimuat. Tekan L untuk membukanya.",
+    kill_minutes = "<R>Kemampuan kamu dimatikan dalam %s menit.",
+    permbanned = "<r>Kamu diblokir secara permanen di #parkour.",
+    tempbanned = "<r>Kamu diblokir dari #parkour selama %s menit.",
+
+    -- Miscellaneous
+    options = "<p align='center'><font size='20'>Opsi Parkur</font></p>\n\nGunakan partikel untuk checkpoin\n\nGunakan keyboard <b>QWERTY</b> (nonaktifkan jika <b>AZERTY</b>)\n\nGunakan <b>M</b> hotkey untuk <b>/mort</b> (nonaktifkan untuk <b>DEL</b>)\n\nMelihat cooldown kemampuan kamu\n\nMelihat tombol kemampuan\n\nMelihat tombol bantuan\n\nMelihat pengumuman penyelesaian peta\n\nMelihatkan simbol tidak memerlukan bantuan",
+    unknown = "Tidak diketahui",
+    powers = "Kemampuan",
+    press = "<vp>Tekan %s",
+    click = "<vp>Klik kiri",
+    ranking_pos = "Peringkat #%s",
+    completed_maps = "<p align='center'><BV><B>Peta diselesaikan: %s</B></p></BV>",
+    leaderboard = "Papan Peringkat",
+    position = "Posisi",
+    username = "Nama panggilan",
+    community = "Komunitas",
+    completed = "Peta diseselesaikan",
+    overall_lb = "Keseluruhan",
+    weekly_lb = "Mingguan",
+    new_lang = "<v>[#] <d>Bahasa diubah ke Bahasa Indonesia",
+    cooldown = "<v>[#] <r>Wow, selamat menikmati! Tunggu beberapa detik untuk melakukannya lagi.",
+
+    -- Power names
+    balloon = "Balon",
+    masterBalloon = "Master Ballon",
+    bubble = "Gelembung",
+    fly = "Terbang",
+    snowball = "Bola Salju",
+    speed = "Speed",
+    teleport = "Teleport",
+    smallbox = "Kotak kecil",
+    cloud = "Awan",
+    rip = "Batu Nisan",
+    choco = "Papan Cokelat",
+    bigBox = "Kotak besar",
+    trampoline = "Trampolin",
+    toilet = "Toilet"
+}
