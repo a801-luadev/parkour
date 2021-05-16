70dcdb3b4e09f1921f5e22ece8a47659142f6536
Blank3495 <meltfruit@gmail.com>
2021-05-16 17:44:50 -0300

    [build] ty for bugfinding #2

--- modes/parkour/chat-ui.lua
+++ modes/parkour/chat-ui.lua
@@ -436,7 +436,7 @@ onEvent("ParsedChatCommand", function(player, cmd, quantity, args)
 end)
 
 onEvent("ColorPicked", function(id, player, color)
-	if not perms[player].set_name_color then return end
+	if not perms[player] or not perms[player].set_name_color then return end
 	if color == -1 then return end
 
 	for name, data in next, room.playerList do
