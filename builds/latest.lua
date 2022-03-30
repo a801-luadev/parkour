d5c0eaa6af14fbeba94a446388fe19ea379710e8
Auto Builder <action@github.com>
2022-03-30 15:06:45 +0000

    Generate build

--- builds/latest.lua
+++ builds/latest.lua
@@ -10333,11 +10333,11 @@ local function initialize_parkour() -- so it uses less space after building
 				end):setPosition(72, 300):setSize(155, 20)
 			):loadComponent( -- Weekly button
 				Button.new():setText("Disabled :D")
-				--[[ Button.new():setTranslation("weekly_lb")
+				--Button.new():setTranslation("weekly_lb")
 
 				:onClick(function(self, player)
 					local args = self.parent.args[player]
-					self.parent:update(player, weekleaderboard, 0, true)
+					--self.parent:update(player, weekleaderboard, 0, true)
 				end)
 
 				:canUpdate(true):onUpdate(function(self, player, data, page, weekly)
@@ -10346,7 +10346,7 @@ local function initialize_parkour() -- so it uses less space after building
 					else
 						self:enable(player)
 					end
-				end) ]]
+				end)
 
 				:setPosition(242, 300):setSize(155, 20)
 			)
