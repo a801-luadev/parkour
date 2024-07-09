bd5a56f8103d01faa63ae4bdc4d0fa8cb9c5e1c3
webninjasi <webninjasi.0@gmail.com>
2024-07-10 02:52:59 +0300

    Log !setrank

--- modes/parkour/moderation.lua
+++ modes/parkour/moderation.lua
@@ -635,6 +635,7 @@ local function handleSetrank(player, cmd, quantity, args)
 	schedule(1, true, function(data)
 		data.ranks[targetPlayer] = ID
 	end)
+	logCommand(player, cmd, math.min(quantity, 2), args)
 end
 
 local function handleBanInfo(player, cmd, quantity, args)
