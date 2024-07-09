d798f05f1eb3b0c2408c47da9b635abda1ba71dd
webninjasi <webninjasi.0@gmail.com>
2024-07-10 01:14:17 +0300

    Bots can receive file, pdata, all incoming packets

---
+++ global/packets.lua
@@ -0,0 +1,24 @@
+local packets = {}
+
+packets.rooms = {
+  crash = 0,
+  hourly_record = 3,
+  weekly_reset = 4,
+  pw_response = 5,
+  command_log = 7,
+  poll_answer = 8,
+  ban_logs = 9,
+  kill_logs = 10,
+  report = 11,
+}
+
+packets.bots = {
+  join = 0,
+  game_update = 1,
+  update_pdata = 2,
+  ban = 3,
+  announce = 4,
+  cm_announce = 5,
+  pw_request = 6,
+  room_announce = 7,
+}
