a8d76afe278e0994386419287d8537e96daa4499
webninjasi <webninjasi.0@gmail.com>
2024-11-29 00:13:27 +0300

    Add more details in hourly record packet

--- modes/parkour/powers.lua
+++ modes/parkour/powers.lua
@@ -1094,7 +1094,15 @@ onEvent("PlayerWon", function(player)
 
 		if hour_count >= 30 and hour_count % 5 == 0 then
 			if hour_count >= 35 then
-				sendPacket("common", 3, room.shortName .. "\000" .. room.playerList[player].id .. "\000" .. player .. "\000" .. hour_count)
+				sendPacket("common", packets.rooms.hourly_record,
+					room.shortName .. "\000" ..
+					room.playerList[player].id .. "\000" ..
+					player .. "\000" ..
+					hour_count .. "\000" ..
+					(room.uniquePlayers or 1) .. "\000" ..
+					room.currentMap .. "\000" ..
+					(room.xmlMapInfo and room.xmlMapInfo.permCode or -1)
+				)
 			end
 
 			local badge = math.ceil((hour_count - 29) / 5)
