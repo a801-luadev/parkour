8213be26061a0ed9ea9001c8d1587e42fac3a06e
webninjasi <webninjasi.0@gmail.com>
2025-01-04 03:11:38 +0300

    Timers: support 6 params

--- modes/parkour/timers.lua
+++ modes/parkour/timers.lua
@@ -4,17 +4,17 @@ do
 local timers = {}
 local aliveTimers = false
 
-function addNewTimer(delay, fnc, arg1, arg2, arg3, arg4, arg5)
+function addNewTimer(delay, fnc, arg1, arg2, arg3, arg4, arg5, arg6)
 	aliveTimers = true
 	local list = timers[delay]
 	if list then
 		list._count = list._count + 1
-		list[list._count] = {os.time() + delay, fnc, arg1, arg2, arg3, arg4, arg5}
+		list[list._count] = {os.time() + delay, fnc, arg1, arg2, arg3, arg4, arg5, arg6}
 	else
 		timers[delay] = {
 			_count = 1,
 			_pointer = 1,
-			[1] = {os.time() + delay, fnc, arg1, arg2, arg3, arg4, arg5}
+			[1] = {os.time() + delay, fnc, arg1, arg2, arg3, arg4, arg5, arg6}
 		}
 	end
 end
@@ -30,7 +30,7 @@ onEvent("Loop", function()
 				timer = list[index]
 
 				if now >= timer[1] then
-					timer[2](timer[3], timer[4], timer[5], timer[6], timer[7])
+					timer[2](timer[3], timer[4], timer[5], timer[6], timer[7], timer[8])
 					newPointer = index + 1
 				else
 					break
@@ -51,13 +51,13 @@ onEvent("NewGame", function()
 			count = list._count
 			for index = list._pointer, count do
 				timer = list[index]
-				timer[2](timer[3], timer[4], timer[5], timer[6], timer[7])
+				timer[2](timer[3], timer[4], timer[5], timer[6], timer[7], timer[8])
 			end
 
 			if list._count > count then
 				for index = count + 1, list._count do
 					timer = list[index]
-					timer[2](timer[3], timer[4], timer[5], timer[6], timer[7])
+					timer[2](timer[3], timer[4], timer[5], timer[6], timer[7], timer[8])
 				end
 			end
 		end
