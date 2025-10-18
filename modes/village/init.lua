2832fb787f2558edcfae3124702aff0e958003d0
webninjasi <webninjasi.0@gmail.com>
2025-10-18 20:39:02 +0300

    Increase power cooldown & player limit in village

--- modes/village/init.lua
+++ modes/village/init.lua
@@ -1,3 +1,5 @@
+tfm.exec.newGame = function() end
+
 onEvent("NewPlayer", function(player, init)
   victory[player] = 0
   setNameColor(player)
@@ -7,4 +9,10 @@ onEvent("Loop", function(time, remaining)
   if remaining < 10000 then
     tfm.exec.setGameTime(3600)
   end
+end)
+
+onEvent("GameStart", function()
+  cooldownMultiplier = 2
+  room_max_players = 200
+  tfm.exec.setRoomMaxPlayers(room_max_players)
 end)
