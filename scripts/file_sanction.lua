10bb4f24795745a493dd70a66b571e8249c7cc6b
webninjasi <webninjasi.0@gmail.com>
2025-12-04 12:49:36 +0300

    Migration script for shop bugfix and sanctions

--- scripts/file_sanction.lua
+++ scripts/file_sanction.lua
@@ -1,22 +1,28 @@
 local sanction = {}
+local concat = table.concat
 
 for i=1, 2000 do
-  sanction[tostring(i)] = {
-    timestamp = os.time() + math.random(-100000, 10000),
-    time = os.time() + 1000 * 60 * 60 * 24 * math.random(1, 30),
-    info = 0,
-    level = 1,
-  }
+  sanction[i] = concat({
+    tostring(i),
+    os.time() + math.random(-100000, 10000),
+    os.time() + 1000 * 60 * 60 * 24 * math.random(1, 30),
+    1,
+    1,
+  }, '\2')
 end
 
-sanction["5419276"] = {
-  timestamp = os.time() + math.random(-100000, 10000),
-  time = os.time() - 1000,
-  info = 0,
-  level = 1,
-}
+sanction[2001] = concat({
+  "5419276",
+  os.time() + math.random(-100000, 10000),
+  os.time() - 1000,
+  1,
+  1,
+}, '\2')
 
-saveFile(43, {
-  mods = { 'Lays#1146' },
-  sanction = sanction,
+saveFile(63, {
+  sanction = {
+    ts = os.time(),
+    mods = { 'Lays#1146' },
+    data = concat(sanction, '\1'),
+  },
 })
--- scripts/migrate.lua
+++ scripts/migrate.lua
@@ -1,7 +1,6 @@
 -- File Migration Script 
 
 {% require-package "tech/filemanager" %}
-{% require-file "modes/parkour/sanctionfilemanager.lua" %}
 {% require-file "modes/parkour/filemanagers.lua" %}
 
 local function saveFile(id, data)
@@ -98,7 +97,94 @@ function migrate_leaderboard()
   system.loadFile(prevId)
 end
 
+function fix_shop_2512()
+  local file_id = 54
+  local item_id = 2007
+  local concat = table.concat
+
+  function eventLoop()end
+  
+  function eventFileLoaded(fileNumber, fileData)
+    if file_id == tonumber(fileNumber) then
+      local data = filemanagers[tostring(file_id)]:load(fileData)
+      local skins = data.shop.skins
+      local parts, count, max_id = {}, 0, 0
+      for part in skins:gmatch('[^\1]+') do
+        local id = tonumber(part:match("^([^\2]+)"))
+        if id ~= item_id then
+          count = count + 1
+          parts[count] = part
+          if id > max_id then
+            max_id = id
+          end
+        else
+          tfm.exec.chatMessage("Removed shop item " .. tostring(item_id) .. " from shop file.")
+        end
+      end
+      tfm.exec.chatMessage("Prev last_id is " .. tostring(data.shop.last_id) .. ".")
+      data.shop.skins = table.concat(parts, '\1')
+      data.shop.last_id = max_id
+      tfm.exec.chatMessage("New last_id is " .. tostring(max_id) .. ".")
+      saveFile(file_id, data)
+    end
+  end
+
+  system.loadFile(file_id)
+end
+
+function migrate_sanctions_2512()
+  {% require-file "modes/parkour/sanctionfilemanager.lua" %}
+
+  local prevId = 43
+  local newId = 63
+  local loaded_data = nil
+
+  tfm.exec.setRoomMaxPlayers(1)
+
+  function eventLoop()end
+  
+  function eventFileLoaded(fileNumber, fileData)
+    if prevId == tonumber(fileNumber) then
+      loaded_data = SanctionFileManager:load(fileData, true)
+      tfm.exec.chatMessage("Loaded sanction data of " .. tostring(#loaded_data.sanction) .. " bytes.")
+    end
+  end
+
+  function eventChatCommand(playerName, command)
+    if command == "load" then
+      system.loadFile(prevId)
+    elseif command == "save" then
+      if not loaded_data then
+        tfm.exec.chatMessage("No data loaded.")
+        return
+      end
+
+      local data, count = {}, 0
+
+      for id, sanction in next, loaded_data.sanction do
+        count = count + 1
+        data[count] = concat({
+          id,
+          sanction.timestamp,
+          sanction.time,
+          sanction.info,
+          sanction.level,
+        }, "\2")
+      end
+
+      saveFile(newId, {
+        ts = os.time(),
+        mods = loaded_data.mods,
+        data = concat(data, '\1'),
+      })
+
+      tfm.exec.chatMessage("Saved file: " .. tostring(#loaded_data.mods) .. " mods, " .. tostring(count) .. " sanctions")
+    end
+  end
+end
 
 --init_shop()
 --init_npc()
 --migrate_leaderboard()
+--fix_shop_2512()
+--migrate_sanctions_2512()
